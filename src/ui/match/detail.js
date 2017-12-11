9f584057d7cd605a88323529ca6ec1bad9372503
Jeremy School <jschool504@gmail.com>
2017-12-10 23:20:19 -0500

    Enable Scrolling in Match Detail View (#26)

--- src/ui/match/detail.js
+++ src/ui/match/detail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 import MatchService from '../../services/match-service';
 
@@ -25,30 +25,32 @@ export default class MatchDetail extends Component {
   render () {
     const match = this.props.navigation.state.params;
     return (
-      <View style={{ marginLeft: 10, marginRight: 10 }}>
-        <Text style={{ fontSize: STANDARD_TEXT_SIZE + 10 }}>{`Total: ${getTotalScore(match)}`}</Text>
-        <View>
-          {match.data.categories.map((category, i) => {
-            return (
-              <View key={i} style={{ marginBottom: 10 }}>
-                <Text style={{ fontSize: STANDARD_TEXT_SIZE, fontWeight: 'bold' }}>{category.name}</Text>
-                <View>
-                  {category.rules.map((rule, ri) => (
-                    <View key={ri} style={{ flexDirection: 'row', marginBottom: 5 }}>
-                      <Text style={{ flex: 5, fontSize: STANDARD_TEXT_SIZE }}>{rule.name}</Text>
-                      <Text style={{ flex: 1, textAlign: 'right' }}>{rule.points}</Text>
-                    </View>
-                  ))}
+      <ScrollView>
+        <View style={{ marginLeft: 10, marginRight: 10 }}>
+          <Text style={{ fontSize: STANDARD_TEXT_SIZE + 10 }}>{`Total: ${getTotalScore(match)}`}</Text>
+          <View>
+            {match.data.categories.map((category, i) => {
+              return (
+                <View key={i} style={{ marginBottom: 10 }}>
+                  <Text style={{ fontSize: STANDARD_TEXT_SIZE, fontWeight: 'bold' }}>{category.name}</Text>
+                  <View>
+                    {category.rules.map((rule, ri) => (
+                      <View key={ri} style={{ flexDirection: 'row', marginBottom: 5 }}>
+                        <Text style={{ flex: 5, fontSize: STANDARD_TEXT_SIZE }}>{rule.name}</Text>
+                        <Text style={{ flex: 1, textAlign: 'right' }}>{rule.points}</Text>
+                      </View>
+                    ))}
+                  </View>
                 </View>
-              </View>
-            );
-          })}
+              );
+            })}
+          </View>
+          <View>
+            <Text style={{ fontSize: STANDARD_TEXT_SIZE, fontWeight: 'bold' }}>{'Comments'}</Text>
+            <Text style={{ fontSize: STANDARD_TEXT_SIZE }}>{match.comments}</Text>
+          </View>
         </View>
-        <View>
-          <Text style={{ fontSize: STANDARD_TEXT_SIZE, fontWeight: 'bold' }}>{'Comments'}</Text>
-          <Text style={{ fontSize: STANDARD_TEXT_SIZE }}>{match.comments}</Text>
-        </View>
-      </View>
+      </ScrollView>
     );
   }
 }

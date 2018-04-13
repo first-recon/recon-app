70584dc553bbd06c7c8e8994925eed1bcf81153d
Jeremy School <jschool504@gmail.com>
2018-04-12 23:39:54 -0400

    19 - Create Filterable List for Tournaments (#21)
    * Added filterable list for current tournament selection
    
    * updated version

---
+++ src/ui/settings/components/tournament-entry.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { TouchableHighlight, Text } from 'react-native';
+
+export default function TournamentEntry ({ tournament, onPress }) {
+  return (
+    <TouchableHighlight style={{ height: 45, marginLeft: 10, justifyContent: 'center' }} onPress={onPress}>
+      <Text>{tournament.name}</Text>
+    </TouchableHighlight>
+  );
+};
